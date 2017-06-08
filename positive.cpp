917a7fb2357d3da41cc52bd664ea4513c2123892
suganthraj <suganthraj96@gmail.com>
2017-06-08 22:30:09 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0) cout<<"positive";
+	if(n<0) cout<<"negative";
+	if(n==0) cout<<"zero";
+	// your code goes here
+	return 0;
+}
