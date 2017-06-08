0abfdf7139eade4b20b8d0acb2d06753aabe7787
suganthraj <suganthraj96@gmail.com>
2017-06-08 22:57:24 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char n;
+	cin>>n;
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u') cout<<"vowel";
+	else cout<<"consonant"
+	
+	// your code goes here
+	return 0;
+}
